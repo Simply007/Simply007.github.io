de532fc0c8865f1526a039d23414dc6802a7632a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-24 11:29:18 +0000

    Deploy Simply007/Simply007.github.io to github.com/Simply007/Simply007.github.io.git:master

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d9ae2e94daa70c2c7611.js"
+    "url": "webpack-runtime-9cb26268ba7c08a71278.js"
   },
   {
     "url": "framework-a38040c4749cf445737f.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8fa404ae75ddc5f509b8ec8c7d3b7d4e"
+    "revision": "4a1d567d4f995843b2b7409e2645c4be"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0097d26fbd474b34ff9b.js"
