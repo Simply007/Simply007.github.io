a87f210d0936f15062a8afdf71cf08444215dc62
ondrejch <ondrejch@kentico.com>
2020-07-31 11:40:19 +0200

    lint

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,4 @@
-
 require('dotenv').config()
-
 
 module.exports = {
   siteMetadata: {
@@ -25,11 +23,17 @@ module.exports = {
       options: {
         projectId: process.env.KONTENT_PROJECT_ID, // Fill in your Project ID
         // if false used authorization key for secured API
-        usePreviewUrl: process.env.KONTENT_PREVIEW_ENABLED && process.env.KONTENT_PREVIEW_ENABLED.toLowerCase() === 'true',
-        authorizationKey: process.env.KONTENT_PREVIEW_ENABLED && process.env.KONTENT_PREVIEW_ENABLED.toLowerCase() === 'true'
-          ? process.env.KONTENT_PREVIEW_KEY
-          : process.env.KONTENT_SECURED_KEY,
-        languageCodenames: process.env.KONTENT_LANGUAGE_CODENAMES.split(',').map(lang => lang.trim()),
+        usePreviewUrl:
+          process.env.KONTENT_PREVIEW_ENABLED &&
+          process.env.KONTENT_PREVIEW_ENABLED.toLowerCase() === 'true',
+        authorizationKey:
+          process.env.KONTENT_PREVIEW_ENABLED &&
+          process.env.KONTENT_PREVIEW_ENABLED.toLowerCase() === 'true'
+            ? process.env.KONTENT_PREVIEW_KEY
+            : process.env.KONTENT_SECURED_KEY,
+        languageCodenames: process.env.KONTENT_LANGUAGE_CODENAMES.split(
+          ','
+        ).map(lang => lang.trim()),
       },
     },
     'gatsby-plugin-sass',
