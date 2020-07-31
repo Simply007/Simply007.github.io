d7a4700f347d9e9a913a3bbf3d034a068d965058
ondrejch <ondrejch@kentico.com>
2020-07-31 13:04:52 +0200

    lint

--- src/templates/journal-item.js
+++ src/templates/journal-item.js
@@ -15,9 +15,7 @@ const JournalItem = ({ data: { kontentItemGotcha } }) => (
         <div className="inner">
           <RichTextElement
             value={kontentItemGotcha.elements.content.value}
-            linkedItems={
-              kontentItemGotcha.elements.content.modular_content
-            }
+            linkedItems={kontentItemGotcha.elements.content.modular_content}
             resolveLinkedItem={linkedItem => {
               switch (linkedItem.__typename) {
                 case 'kontent_item_code_snippet':
