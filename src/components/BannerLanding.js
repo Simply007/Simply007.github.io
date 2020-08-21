930a765ffa018dd7f6a2a6659c476ca42a5f5347
ondrejch <ondrejch@kentico.com>
2020-08-21 20:06:33 +0200

    add back putton to banner and use it on journal

--- src/components/BannerLanding.js
+++ src/components/BannerLanding.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const BannerLanding = props => (
-  <section id="banner" className="style2">
+  <section id="banner" className={`style${Math.floor(Math.random() * 6) + 1} alt`}>
     <div className="inner">
       <header className="major">
         <h1>{props.title}</h1>
@@ -10,6 +11,16 @@ const BannerLanding = props => (
         className="content"
         dangerouslySetInnerHTML={{ __html: props.content }}
       ></div>
+      {props.button && (
+        <div className="content">
+          <Link
+            className="button icon fa-angle-double-left"
+            to={props.button.to}
+          >
+            {props.button.title}
+          </Link>
+        </div>
+      )}
     </div>
   </section>
 )
