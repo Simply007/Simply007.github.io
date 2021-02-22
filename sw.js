32a1ade8d5f399ef6a2a5d05cb64965ec4dd29cd
Simply007 <9218736+Simply007@users.noreply.github.com>
2021-02-22 13:46:02 +0000

    Deploying to master from  @ 47ac632b16a1e3add06deea81d57e653fba6bbbb 🚀

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-7b04b82a0a48d24bd318.js"
+    "url": "webpack-runtime-d7375b416527ad56af5b.js"
   },
   {
     "url": "framework-2d6b7d2e2be378715b78.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d48abf59dac9216d95e1764f29181f33"
+    "revision": "9a2cc7606368c23006e63fc20654f88e"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-fd4fb51a6fac1c18bdde.js"
