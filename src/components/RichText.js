b4a6d633c59360a94d89baab8a1c7acbcb6c74fc
Ondřej Chrastina <9218736+Simply007@users.noreply.github.com>
2025-02-18 17:37:33 +0100

    internal link resolution for rich text

--- src/components/RichText.js
+++ src/components/RichText.js
@@ -8,7 +8,7 @@ const RichText = ({ element }) => (
     linkedItems={element.modular_content}
     resolveLinkedItem={(linkedItem) => {
       switch (linkedItem.system.type) {
-        case 'code_snippet':
+        case 'code_snippet': {
           const codeComponent = JSON.parse(linkedItem.elements.code.value)
           return (
             <CodeHighlighter
@@ -16,21 +16,81 @@ const RichText = ({ element }) => (
               code={codeComponent.code}
             />
           )
-        case 'button':
-          return (
-            <p>
-              <a
-                href={linkedItem.elements.external_url.value}
-                className="button"
-              >
-                <span className={`icon ${linkedItem.elements.icon.value}`}>
-                  {linkedItem.elements.title.value}
-                </span>
-              </a>
-            </p>
-          )
-        default:
+        }
+        case 'button': {
+          if (linkedItem.elements.external_url.value) {
+            return (
+              <p>
+                <a
+                  href={linkedItem.elements.external_url.value}
+                  className="button"
+                >
+                  <span className={`icon ${linkedItem.elements.icon.value}`}>
+                    {linkedItem.elements.title.value}
+                  </span>
+                </a>
+              </p>
+            )
+            // link existing content item
+          } else if (linkedItem.elements.link_to.value.length > 0) {
+            debugger;
+            const linkedContentItem = linkedItem.elements.link_to.value[0]
+            const title = linkedItem.elements.title.value || linkedContentItem.elements.title.value;
+            switch (linkedContentItem.__typename) {
+              case 'kontent_item_navigation_item': {
+                return (
+                  <p>
+                    <a
+                      href={`/${linkedContentItem.elements.slug.value}`}
+                      className="button"
+                    >
+                      <span className={`icon ${linkedItem.elements.icon.value}`}>
+                        {title}
+                      </span>
+                    </a>
+                  </p>
+                )
+              }
+              case 'kontent_item_project': {
+                const iconCode = linkedItem.elements.icon.value || 'fa-project-diagram';
+                return (
+                  <p>
+                    <a
+                      href={`/projects/${linkedContentItem.elements.url_slug.value}`}
+                      className="button"
+                    >
+                      <span className={`icon ${iconCode}`}>
+                        {title}
+                      </span>
+                    </a>
+                  </p>
+                )
+              }
+              case 'kontent_item_gotcha': {
+                const iconCode = linkedItem.elements.icon.value || 'fa-blog';
+                return (
+                  <p>
+                    <a
+                      href={`/gotchas/${linkedContentItem.elements.url_slug.value}`}
+                      className="button"
+                    >
+                      <span className={`icon ${iconCode}`}>
+                        {title}
+                      </span>
+                    </a>
+                  </p>
+                )
+              }
+              default: {
+                return <div>Component not supported</div>
+              }
+            }
+          }
           return <div>Component not supported</div>
+        }
+        default: {
+          return <div>Component not supported</div>
+        }
       }
     }}
     images={element.images}
